10f298c15294d1866ce4c8c228fba4b8fc89e13f
Yuvraj Goyal <68333785+mrManiac-uv@users.noreply.github.com>
2023-06-01 13:40:03 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import Header from "./components/Header";
 import Planner from "./pages/Planner";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
